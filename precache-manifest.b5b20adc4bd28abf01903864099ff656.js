af1ce5f9a755a69bc972f63d64b6d9cb9dfcd06f
julienbigras <julien.bigras@hotmail.com>
2019-06-03 11:00:21 -0400

    Updates

---
+++ precache-manifest.b5b20adc4bd28abf01903864099ff656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318ef19cda8019e5eeab57a7342405f7",
+    "url": "/julien-bigras-project-5/index.html"
+  },
+  {
+    "revision": "9a56f45b70e34f822bfb",
+    "url": "/julien-bigras-project-5/static/css/main.e6143695.chunk.css"
+  },
+  {
+    "revision": "e5fa0332dee56d079315",
+    "url": "/julien-bigras-project-5/static/js/2.73b85389.chunk.js"
+  },
+  {
+    "revision": "9a56f45b70e34f822bfb",
+    "url": "/julien-bigras-project-5/static/js/main.7ec8c6fc.chunk.js"
+  },
+  {
+    "revision": "cec3d2ae59e213f47735",
+    "url": "/julien-bigras-project-5/static/js/runtime~main.4b116e87.js"
+  }
+]);
